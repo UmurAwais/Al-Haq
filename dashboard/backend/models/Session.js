e97b5f1192b2820bb4783fc87b5bf7e06882f0d6
Umar Awais <umarawais.co@gmail.com>
2026-03-10 13:56:42 +0500

    Remove backend folder from frontend repo

--- dashboard/backend/models/Session.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const sessionSchema = new mongoose.Schema({
-  uid: { type: String, required: true, unique: true },
-  sessionId: { type: String, required: true },
-  lastActive: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("Session", sessionSchema);
